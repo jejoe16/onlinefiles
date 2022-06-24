b09eea6fef04b9f2995f85c6156571b9267d7200
jkj <jkj@rpa-solutions.dk>
2022-06-24 12:56:36 +0200

    www

---
+++ www/precache-manifest.2d26414cf0bceec3802a8080e5af3f0d.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "83bf82c222eb350520be",
+    "url": "/js/about.922fa709.js"
+  },
+  {
+    "revision": "f8779b9f53e4300504c4",
+    "url": "/css/app.c29205c8.css"
+  },
+  {
+    "revision": "f8779b9f53e4300504c4",
+    "url": "/js/app.fa5270ca.js"
+  },
+  {
+    "revision": "2c7ed248effbb037b984",
+    "url": "/css/chunk-vendors.498176c8.css"
+  },
+  {
+    "revision": "2c7ed248effbb037b984",
+    "url": "/js/chunk-vendors.43090654.js"
+  },
+  {
+    "revision": "cf0b6b88896bb450bb94684ecaf35f74",
+    "url": "/img/features-3.cf0b6b88.svg"
+  },
+  {
+    "revision": "cf9c451245634f1a63ba2588fafe3310",
+    "url": "/img/hero-bg.cf9c4512.jpg"
+  },
+  {
+    "revision": "be57951d47bc6b1635653a38cbdf30fd",
+    "url": "/fonts/bootstrap-icons.be57951d.woff2"
+  },
+  {
+    "revision": "bad01a8965b8562e2e7447fe04723acd",
+    "url": "/fonts/bootstrap-icons.bad01a89.woff"
+  },
+  {
+    "revision": "d9177fbc6284f3221258ea6b0b4eb126",
+    "url": "/fonts/boxicons.d9177fbc.woff2"
+  },
+  {
+    "revision": "82c9de79b0675cc8757b080311775011",
+    "url": "/fonts/boxicons.82c9de79.eot"
+  },
+  {
+    "revision": "1d0805b54db29521cb57e661fb51b91c",
+    "url": "/fonts/boxicons.1d0805b5.woff"
+  },
+  {
+    "revision": "ffe87d4821ad790a70dbc2849d9b9866",
+    "url": "/fonts/boxicons.ffe87d48.ttf"
+  },
+  {
+    "revision": "f962c3205f70ff47e6a86928d925e49f",
+    "url": "/img/boxicons.f962c320.svg"
+  },
+  {
+    "revision": "52980d42775d44be0f030f0616dc88f5",
+    "url": "/img/features-1.52980d42.svg"
+  },
+  {
+    "revision": "a961f4486cba78098f0bb17e5397259c",
+    "url": "/img/features-4.a961f448.svg"
+  },
+  {
+    "revision": "529facd4cee1d1f9f4d79370a555049e",
+    "url": "/img/features-2.529facd4.svg"
+  },
+  {
+    "revision": "6f6bbb16aec97391aefe120ec5a4e6a2",
+    "url": "/img/user.6f6bbb16.png"
+  },
+  {
+    "revision": "015fb4eb0dd4fb2c22ca7a27d5ab6038",
+    "url": "/img/section-background.015fb4eb.svg"
+  },
+  {
+    "revision": "7357745905a0c238489fee6097c9662c",
+    "url": "/fonts/ProximaNova-Light-webfont.73577459.woff2"
+  },
+  {
+    "revision": "b7f2ac1eb78d290c7dae4c6833fbed3d",
+    "url": "/fonts/ProximaNova-Light-webfont.b7f2ac1e.woff"
+  },
+  {
+    "revision": "577dba6d98ff00ebbc9e5f51182b3376",
+    "url": "/fonts/ProximaNova-Reg-webfont.577dba6d.woff"
+  },
+  {
+    "revision": "2b15976547d0476798183e90246a6c46",
+    "url": "/fonts/ProximaNova-Sbold-webfont.2b159765.woff2"
+  },
+  {
+    "revision": "e382c667a81127659f464991b4013dfd",
+    "url": "/fonts/ProximaNova-Reg-webfont.e382c667.woff2"
+  },
+  {
+    "revision": "15c24f7109941777774ddd2c636c6a50",
+    "url": "/fonts/montserrat-600.15c24f71.woff2"
+  },
+  {
+    "revision": "e325aaeb392e1215ea4cb321502b91e6",
+    "url": "/fonts/ProximaNova-Sbold-webfont.e325aaeb.woff"
+  },
+  {
+    "revision": "e92cc0fb9e1a7debc138224fd02a462a",
+    "url": "/fonts/roboto-mono-regular.e92cc0fb.woff2"
+  },
+  {
+    "revision": "f6dc6096f48956908c1787d9a722570a",
+    "url": "/fonts/montserrat-600.f6dc6096.woff"
+  },
+  {
+    "revision": "1db1469bf7acbd08f97df4f18046a50f",
+    "url": "/fonts/roboto-mono-regular.1db1469b.woff"
+  },
+  {
+    "revision": "a5f5b2c446be59906ab00886724ff623",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
